df7f73c4dbd7f76e0b6409a4e312d1232dda74b6
Cameron Cabo <ccabo@seas.upenn.edu>
2017-05-20 12:31:26 -0700

    Added to projects links

---
+++ projects/js/md_script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#title').delay(1000).fadeIn(500);
+    $('#1').delay(2500).fadeIn(500);
+    $('#2').delay(3000).fadeIn(500);
+    $('#3').delay(3500).fadeIn(500);
+    $('#ty').delay(5000).fadeIn(500);
+});
